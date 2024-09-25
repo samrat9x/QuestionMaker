9d4777ef78fee5b0713a738542d9e5422e21201a
Samrat <67840234+samrat9x@users.noreply.github.com>
2024-09-25 21:44:12 +0600

    fe

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ let printBtn = document.querySelector('#printBtn');
             }
 
             if (totalMarks === totalMarksInput) {
-                questionPaper.innerHTML = `<h2>${document.getElementById('subjectSelect').value} (মোট নম্বর: ${totalMarksInput})</h2>`;
+                questionPaper.innerHTML = `<h2>${document.getElementById('subjectSelect').value} (Total Marks: ${totalMarksInput})</h2>`;
                 selectedQuestions.forEach((questionObj, index) => {
                     questionPaper.innerHTML += `<p>${index + 1}. ${questionObj.question} (${questionObj.marks} marks)</p>`;
                 });
